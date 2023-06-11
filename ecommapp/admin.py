db5d11316fb2a7a23421b7c48b9d3bed66bf7aa7
sujal-jadhav-79 <sujaljadhav2732@gmail.com>
2023-06-11 21:18:46 +0530

    First commit

---
+++ ecommapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from ecommapp.models import Product
+
+# Register your models here.
+# admin.site.register(Product)
+
+# Define model admin class
+class ProductAdminClass(admin.ModelAdmin):
+   list_display=['name','cat','price','status']
+   list_filter=['status','cat']
+
+admin.site.register(Product,ProductAdminClass)
+admin.site.site_header='Ekart Dashboard'
